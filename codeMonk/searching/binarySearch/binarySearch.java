ee9cbeb7940561d1b4cf3eb5b5e435930b6f5af1
sksaifuddin <atushaik@gmail.com>
2020-02-18 06:40:40 +0530

    corrected the iterative algorithm for binary search

--- codeMonk/searching/binarySearch/binarySearch.java
+++ codeMonk/searching/binarySearch/binarySearch.java
@@ -1,31 +1,26 @@
 import java.util.Scanner;
 
-class binarySearch {
+class BinarySearch {
   public static void main(String args[]) {
-      Scanner sc = new Scanner(System.in);
-      int res=-1;
-      int max_size = sc.nextInt();
-      int arr[] = new int[max_size];
-    int low=0; int high = max_size-1;int mid = 0;
-        for(int i=0; i<max_size; i++) {
-            arr[i] = sc.nextInt();
+        int input[] = {2,3,4,5,6};
+        int number = 5;
+        int first = 0;
+        int last = input.length - 1;
+        int middle = (first + last) / 2;
+        while (first <= last) {
+            if (input[middle] < number) {
+                first = middle + 1;
+            } else if (input[middle] == number) {
+                System.out.printf(number + " found at location %d %n", middle);
+                break;
+            } else {
+                last = middle - 1;
+            }
+            middle = (first + last) / 2;
         }
-    int key = sc.nextInt();
-    while(low<=high) {
-        mid = low+(high-low)/2;
-        if(arr[mid] == key) {
-            res = mid;
-        } else if(arr[mid] < key) {
-            low = mid+1;
-        } else {
-           high = mid-1;
+        if (first > last) {
+            System.out.println(number + " is not present in the list.\n");
         }
-
-        break;
-
     }
-    System.out.println("key is at value"+ res);
-
-
+   
   }
-}
